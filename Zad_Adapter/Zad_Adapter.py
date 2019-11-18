5482dc54d23a7e0defa5432109fea816ffd22e2f
Paarzivall <44908654+Paarzivall@users.noreply.github.com>
2019-11-18 10:02:58 +0100

    add Adapter

--- Zad_Adapter/Zad_Adapter.py
+++ Zad_Adapter/Zad_Adapter.py
@@ -1,8 +1,32 @@
 from abc import ABC, abstractmethod
 
-class Figura(ABC):
+
+class XXOkrag(object):
     def __init__(self):
-        self.polozenie = []
+        pass
+
+    def wyswietlaj(self):
+        return f'Jestem Okręgiem'
+
+    def wypelniaj(self):
+        pass
+
+    def usuwaj(self):
+        pass
+
+    def pobierzPolozenie(self):
+        pass
+
+    def nadajPolozenie(self):
+        pass
+
+    def ustawKolor(self):
+        pass
+
+class Figura(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
 
     def pobierzPolozenie(self):
         pass
@@ -14,10 +38,68 @@ class Figura(ABC):
     def wyswietl(self):
         pass
 
-    @abstractmethod
     def wypelnij(self):
         pass
 
-    @abstractmethod
     def usun(self):
         pass
+
+
+class Kwadrat(Figura):
+    def __init__(self):
+        super().__init__()
+
+    def wyswietl(self):
+        return f'Jestem Kwadratem'
+
+
+class Linia(Figura):
+    def __init__(self):
+        super().__init__()
+
+    def wyswietl(self):
+        return f'Jestem Linią'
+
+
+class Punkt(Figura):
+    def __init__(self):
+        super().__init__()
+
+    def wyswietl(self):
+        return f'Jestem Punktem'
+
+
+class Okrag(Figura):
+    def __init__(self):
+        super().__init__()
+        self.xxOkrag = XXOkrag()
+
+    def pobierzPolozenie(self):
+        self.xxOkrag.pobierzPolozenie()
+
+    def nadajPolozenie(self):
+        self.xxOkrag.nadajPolozenie()
+
+    def wyswietl(self):
+        return self.xxOkrag.wyswietlaj()
+
+    def wypelnij(self):
+        self.xxOkrag.wypelniaj()
+
+    def nadajKolor(self):
+        self.xxOkrag.ustawKolor()
+
+    def usun(self):
+        self.xxOkrag.usuwaj()
+
+
+if __name__ == '__main__':
+    punkt = Punkt()
+    linia = Linia()
+    kwadrat = Kwadrat()
+    okrag = Okrag()
+
+    figury = [punkt, linia, kwadrat, okrag]
+
+    for figura in figury:
+        print(figura.wyswietl())
